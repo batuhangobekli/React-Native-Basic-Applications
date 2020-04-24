4fa06ab7710b3416ec2ecc81ecccbbc5d63c04cc
batuhangobekli <btubatuhan@gmail.com>
2020-04-24 16:52:54 +0300

    Flat list done (HWA)

--- 1)Hello World/rn-starter/src/screens/ListScreen.js
+++ 1)Hello World/rn-starter/src/screens/ListScreen.js
@@ -1,32 +1,37 @@
 import React from "react";
-import { Text, StyleSheet,FlatList} from "react-native";
+import { Text, StyleSheet, FlatList, View } from "react-native";
 
 const ListScreen = () => {
-    const friends = [
-        {name:'Friend #1' },
-        {name:'Friend #2' },
-        {name:'Friend #3' },
-        {name:'Friend #4' },
-        {name:'Friend #5' },
-        {name:'Friend #6' },
-    ];
-    return (
-        <FlatList 
-        keyExtractor ={friend => friends.name}
-        data = {friends}
-        renderItem = {
-            ({ item }) => {
-                return <Text>{item.name}</Text>
-            }} 
-        />
-    )
-  };
+  const friends = [
+    { name: "Friend #1", age: 12 },
+    { name: "Friend #2", age: 13 },
+    { name: "Friend #3", age: 14 },
+    { name: "Friend #4", age: 15 },
+    { name: "Friend #5", age: 16 },
+    { name: "Friend #6", age: 17 },
+  ];
+  return (
+    <FlatList
+      // horizontal = {true} Make list horizontal
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(friend) => friends.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
+};
 
-  const styles = StyleSheet.create({
-    text: {
-      fontSize: 30
-    }
-  });
-  
-  export default ListScreen;
-  
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+    fontSize: 15,
+  },
+});
+
+export default ListScreen;
