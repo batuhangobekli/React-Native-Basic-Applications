8a64702da8da6c08a49cf5a9911e2a3989e987e8
batuhangobekli <btubatuhan@gmail.com>
2020-04-24 17:17:51 +0300

    Button example OK

--- 1)Hello World/rn-starter/src/screens/HomeScreen.js
+++ 1)Hello World/rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There!</Text>;
+  return (
+    <View> 
+     <Text style={styles.text}>Hi There!</Text>
+    <Button 
+      onPress = {() => console.log('Button pressed')}
+      title = "sa">
+      </Button>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
