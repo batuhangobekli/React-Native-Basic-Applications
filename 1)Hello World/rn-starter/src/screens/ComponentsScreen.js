c626398b24ecc04a3c2bd1d1ba96dc04d65c90fe
batuhangobekli <btubatuhan@gmail.com>
2020-04-24 14:09:41 +0300

    (HWA) New texts added.

--- 1)Hello World/rn-starter/src/screens/ComponentsScreen.js
+++ 1)Hello World/rn-starter/src/screens/ComponentsScreen.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  const greeting = <Text>Hello thank you!</Text>;
-
+  const name = "Batuhan"
   return (
     <View>
-      <Text style={styles.textStyle}>Hey</Text>
-      {greeting}
+      <Text style = {styles.textStyle}>Getting started with React Native!</Text>
+      <Text style = {styles.subHeaderStyle}>My name {name}</Text>
     </View>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
   },
+  subHeaderStyle: {
+    fontSize: 20,
+  }
 });
 
 export default ComponentsScreen;
