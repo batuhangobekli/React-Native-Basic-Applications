f93640c30fa3b4a4cebb4347d3101cad45daf6d7
batuhangobekli <btubatuhan@gmail.com>
2020-04-28 16:14:41 +0300

    color counter done

--- 1)Hello World/rn-starter/src/screens/SquareScreen.js
+++ 1)Hello World/rn-starter/src/screens/SquareScreen.js
@@ -2,12 +2,61 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
-    return <View>
-        <ColorCounter color= "Red"/>
-        <ColorCounter color= "Green"/>
-        <ColorCounter color= "Blue"/>
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const setColor = (color, change) => {
+    //color === 'red','green','blue'
+    //change === +15, -15
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+    }
+  };
+
+  return (
+    <View>
+      <ColorCounter
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -COLOR_INCREMENT)}
+        color="Red"
+      />
+      <ColorCounter
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() =>  setColor("green", -COLOR_INCREMENT)}
+        color="Green"
+      />
+      <ColorCounter
+        onIncrease={() =>setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -COLOR_INCREMENT)}
+        color="Blue"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: "rgb(" + red + "," + green + "," + blue + ")",
+        }}
+      />
     </View>
+  );
 };
 
 const styles = StyleSheet.create({});
