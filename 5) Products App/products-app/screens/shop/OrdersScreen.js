f9dd002f6036ca470c1e314bd735e6fa8604b559
batuhangobekli <btubatuhan@gmail.com>
2020-06-08 10:35:12 +0300

    Orders screen done

--- 5) Products App/products-app/screens/shop/OrdersScreen.js
+++ 5) Products App/products-app/screens/shop/OrdersScreen.js
@@ -2,7 +2,9 @@ import React from "react";
 import { FlatList, Text, Platform } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import HeaderButton from "../../components/UI/CustomHeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
@@ -11,7 +13,13 @@ const OrdersScreen = (props) => {
     <FlatList
       data={orders}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
--- 5) Products App/products-app/store/reducers/cart.js
+++ 5) Products App/products-app/store/reducers/cart.js
@@ -1,5 +1,6 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
+import { ADD_ORDER } from "../actions/orders";
 
 const initialState = {
   items: {},
@@ -53,6 +54,8 @@ export default (state = initialState, action) => {
         items: updatedCartItems,
         totalAmount: state.totalAmount - selectedCartItem.productPrice,
       };
+    case ADD_ORDER:
+      return initialState;
   }
 
   return state;
